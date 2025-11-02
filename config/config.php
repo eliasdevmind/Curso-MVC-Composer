7a12becb3fec2f8bde28650e10af156b6753ba09
Elias <eliasaraujo1222@outlook.com>
2025-11-02 20:52:49 -0300

    first commit

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de Mvc</title>
+    <link rel="stylesheet" href=".\config/materialize/css/materialize.css">
+</head>
+
+<body>
+    <nav class="blue-grey darken-3"></nav>
+
+
+    <script src=".\config\materialize\js\materialize.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+use core\Router;
+
+require_once __DIR__ . '/vendor/autoload.php';
